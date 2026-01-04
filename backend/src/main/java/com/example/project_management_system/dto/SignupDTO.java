4a5677de8c8ed2ad2d877c1b82e2f62f3d2d9751
NadaAlinour <lime.x.na@gmail.com>
2026-01-05 00:13:38 +0200

    rename userdto to signupdto and update fields

--- backend/src/main/java/com/example/project_management_system/dto/SignupDTO.java
+++ backend/src/main/java/com/example/project_management_system/dto/SignupDTO.java
@@ -1,16 +1,15 @@
 package com.example.project_management_system.dto;
 
 import com.example.project_management_system.entity.Role;
-import com.example.project_management_system.entity.Tenant;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class UserDTO {
+public class SignupDTO {
     private String firstname;
     private String lastname;
     private String email;
-    private Tenant tenant;
-    private Role role;
+    private String password;
+    private Role role; // should default to admin and i think i will handle this in the signupgit s service
 }
